bd6dbdff7b3216aba6c20c0280e4d2f12501d41a
zzq <i@zhangzqs.cn>
2022-10-26 10:12:42 +0800

    update(model/interactive_infinity_container): 优化模块接口

--- models/interactive_infinity_container/lib/interactive_infinity_container.dart
+++ models/interactive_infinity_container/lib/interactive_infinity_container.dart
@@ -6,19 +6,25 @@ import 'infinity_stack.dart';
 
 class InteractiveInfinityContainer extends StatelessWidget {
   final List<Widget> children;
-  final TransformationController viewerTransformationController;
+  final TransformationController? viewerTransformationController;
+  final double maxScale;
+  final double minScale;
   const InteractiveInfinityContainer({
     Key? key,
+    this.maxScale = 2.5,
+    this.minScale = 0.8,
     required this.children,
-    required this.viewerTransformationController,
+    this.viewerTransformationController,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InteractiveViewer(
+      minScale: minScale,
+      maxScale: maxScale,
       transformationController: viewerTransformationController,
       boundaryMargin: const EdgeInsets.all(double.infinity),
-      child: InfinityStack(children: children),
+      child: InfinityStack(children: children.map((e) => Positioned(top: 0, left: 0, child: e)).toList()),
     );
   }
 }
