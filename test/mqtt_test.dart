2c1c7a97a13cfdc3de69231174d1987b4c4cb126
zzq <i@zhangzqs.cn>
2022-10-31 17:18:57 +0800

    test(mqtt): 完善mqtt类的测试用例

--- test/mqtt_test.dart
+++ test/mqtt_test.dart
@@ -1,16 +1,20 @@
 import 'package:board_front/mqtt.dart';
+import 'package:board_front/util/log.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 
 void main() {
   test('test_mqtt', () async {
-    final node = BoardNode(
-      roomId: 'roomId',
-      nodeId: 'nodeId',
-      onModelChanged: (Map<dynamic, dynamic> value) {},
-    );
-    await node.connect();
-    node.broadcast('test', 'message');
-    await MqttUtilities.asyncSleep(60);
+    final node1 = BoardNode(roomId: 'room1', nodeId: '1');
+    await node1.connect();
+    await MqttUtilities.asyncSleep(2);
+    Log.info(node1.onlineList); // [1]
+    final node2 = BoardNode(roomId: 'room1', nodeId: '2');
+    await node2.connect();
+    await MqttUtilities.asyncSleep(2);
+    Log.info(node1.onlineList); // [1,2]
+    node2.disconnect();
+    await MqttUtilities.asyncSleep(6);
+    Log.info(node1.onlineList); // [1]
   });
 }
