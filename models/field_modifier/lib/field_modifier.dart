5d1ab7b47584de46d93f0fbd0ede98aa67be529d
zzq <i@zhangzqs.cn>
2022-10-31 18:09:51 +0800

    refactor & test: 整理field_modifier目录结构与测试用例

--- models/field_modifier/lib/field_modifier.dart
+++ models/field_modifier/lib/field_modifier.dart
@@ -1,3 +1,19 @@
-int calculate() {
-  return 6 * 7;
+class FieldModifier {
+  final dynamic data;
+  FieldModifier(this.data);
+  dynamic _get(dynamic data, List<dynamic> path, int deep) {
+    if (deep >= path.length) return data;
+    return _get(data[path[deep]], path, deep + 1);
+  }
+
+  dynamic get(List<dynamic> path) => _get(data, path, 0);
+  dynamic operator [](List<dynamic> path) => get(path);
+
+  void _set(dynamic data, List<dynamic> path, int deep, dynamic value) {
+    final parent = _get(data, path.sublist(0, path.length - 1), 0);
+    parent[path.last] = value;
+  }
+
+  void set(List<dynamic> path, dynamic value) => _set(data, path, 0, value);
+  void operator []=(List<dynamic> path, dynamic value) => set(path, value);
 }
--- models/field_modifier/test/field_modifier_test.dart
+++ models/field_modifier/test/field_modifier_test.dart
@@ -1,8 +1,36 @@
 import 'package:field_modifier/field_modifier.dart';
 import 'package:test/test.dart';
 
+FieldModifier testMap() => FieldModifier({
+      'a1': {
+        'a2': 123,
+        'b2': true,
+        'c2': [1, 2, 3],
+      },
+      'b1': 123,
+    });
+FieldModifier testList() => FieldModifier([
+      3,
+      4,
+      [2, 3, 4]
+    ]);
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('test map set by path', () {
+    final m = testMap();
+    m[['a1', 'c2', 2]] = 98;
+    expect(m.data['a1']['c2'][2], equals(98));
+  });
+  test('test map get by path', () {
+    final m = testMap();
+    expect(m.data['a1']['b2'], equals(true));
+  });
+  test('test list get by path', () {
+    final m = testList();
+    expect(m.data[2], equals([2, 3, 4]));
+  });
+  test('test list set by path', () {
+    final m = testList();
+    m[[2, 1]] = 999;
+    expect(m.data[2][1], equals(999));
   });
 }
