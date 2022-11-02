6f92b22b33aa55d94340ce7b50078c403e1ce755
zzq <i@zhangzqs.cn>
2022-11-02 20:19:08 +0800

    update: 简化Text组件

--- lib/component/board/model/text/widget.dart
+++ lib/component/board/model/text/widget.dart
@@ -8,18 +8,6 @@ class TextModelWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = TextEditingController();
-    controller.text = data.content;
-    controller.addListener(() {});
-    return EditableText(
-      controller: controller,
-      focusNode: FocusNode(),
-      selectionColor: Colors.blue,
-      style: TextStyle(
-        color: data.color ?? Colors.black,
-      ),
-      cursorColor: Colors.black,
-      backgroundCursorColor: Colors.transparent,
-    );
+    return Text(data.content);
   }
 }
