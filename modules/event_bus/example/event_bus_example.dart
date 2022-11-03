5d2eb0b3812a6ca504b4d50637a5c16cf2792c67
zzq <i@zhangzqs.cn>
2022-11-03 22:34:48 +0800

    refactor(modules/event_bus): 修改api接口名称，添加example

--- modules/event_bus/example/event_bus_example.dart
+++ modules/event_bus/example/event_bus_example.dart
@@ -1,4 +1,24 @@
 import 'package:event_bus/event_bus.dart';
 
+enum EventName {
+  eventA,
+  eventB,
+}
+
 void main() {
+  final bus = EventBus<EventName>();
+  bus.subscribe(EventName.eventA, (arg) {
+    print('eventA subscribe1: $arg');
+  });
+  bus.subscribe(EventName.eventA, (arg) {
+    print('eventA subscribe2: $arg');
+  });
+  bus.subscribe(EventName.eventB, (arg) {
+    print('eventB subscribe1: $arg');
+  });
+  bus.subscribe(EventName.eventB, (arg) {
+    print('eventB subscribe2: $arg');
+  });
+  bus.publish(EventName.eventA);
+  bus.publish(EventName.eventB, 'HelloWorld');
 }
--- modules/event_bus/lib/event_bus.dart
+++ modules/event_bus/lib/event_bus.dart
@@ -9,7 +9,7 @@ typedef EventCallback<T> = void Function(T? arg);
 /// E为事件类型，一般使用枚举类型，也可使用其他类型
 class EventBus<E> {
   /// 保存事件订阅者队列，key:事件名(id)，value: 对应事件的订阅者队列
-  final _eventMap = <E, List<EventCallback>?>{};
+  final _eventQueueMap = <E, List<EventCallback>?>{};
 
   /// 监听事件
   Future<T?> listen<T>(E eventName) {
@@ -17,25 +17,25 @@ class EventBus<E> {
 
     void subscriber(T? arg) {
       completer.complete(arg);
-      off(eventName, subscriber);
+      unsubscribe(eventName, subscriber);
     }
 
-    on<T>(eventName, subscriber);
+    subscribe<T>(eventName, subscriber);
     return completer.future;
   }
 
   /// 添加订阅者
-  void on<T>(E eventName, EventCallback<T> callback) {
+  void subscribe<T>(E eventName, EventCallback<T> callback) {
     // 如果对应事件的队列不存在，就新建一个新队列
-    _eventMap[eventName] ??= <EventCallback>[];
+    _eventQueueMap[eventName] ??= <EventCallback>[];
     // 为队列追加元素，由于类型不统一，所以需要单独嵌套做类型转换
-    _eventMap[eventName]!.add((arg) => callback(arg));
+    _eventQueueMap[eventName]!.add((arg) => callback(arg));
   }
 
   /// 判定订阅者是否存在
-  bool contain<T>(E eventName, [EventCallback<T>? f]) {
+  bool contains<T>(E eventName, [EventCallback<T>? f]) {
     // 获取队列，若该事件不存在，那直接退出函数
-    final list = _eventMap[eventName];
+    final list = _eventQueueMap[eventName];
     if (list == null) {
       return false;
     }
@@ -48,30 +48,30 @@ class EventBus<E> {
   }
 
   /// 移除订阅者
-  void off<T>(E eventName, [EventCallback<T>? f]) {
+  void unsubscribe<T>(E eventName, [EventCallback<T>? f]) {
     // 获取队列，若该事件不存在，那直接退出函数
-    final list = _eventMap[eventName];
+    final list = _eventQueueMap[eventName];
     if (list == null) {
       return;
     }
     // 若回调为空，则删除队列所有值
     if (f == null) {
-      _eventMap[eventName] = null;
+      _eventQueueMap[eventName] = null;
     } else {
       // 不为空时，删除特定回调
       list.remove(f);
     }
   }
 
-  /// 触发事件，事件触发后该事件所有订阅者会被调用
+  /// 发布事件，事件发布后该事件所有订阅者会被调用
   /// T为发布参数类型
-  void emit<T>(E eventName, [T? arg]) {
-    final list = _eventMap[eventName];
+  void publish<T>(E eventName, [T? arg]) {
+    final list = _eventQueueMap[eventName];
     if (list == null) {
       return;
     }
-    // 队列队尾元素为先进元素，需要先调用
-    for (final element in list.reversed) {
+    // 从头至尾依次调用
+    for (final element in list) {
       element(arg);
     }
   }
