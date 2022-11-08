47cae999abf43769c016ed0e8cd8ef43c361b076
jimyag <jiaminyag@gmail.com>
2022-11-08 19:21:48 +0800

    refactor: 优化代码

--- lib/component/board/model/rect/widget.dart
+++ lib/component/board/model/rect/widget.dart
@@ -6,17 +6,6 @@ class RectModelWidget extends StatelessWidget {
   final RectModelData data;
   const RectModelWidget({Key? key, required this.data}) : super(key: key);
 
-  Widget buildText() => Container(
-        alignment: data.text.alignment,
-        child: Text(
-          data.text.content,
-          style: TextStyle(
-            color: data.text.color,
-            fontSize: data.text.fontSize,
-          ),
-        ),
-      );
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,7 +16,16 @@ class RectModelWidget extends StatelessWidget {
           width: data.border.width,
         ),
       ),
-      child: buildText(),
+      child: Container(
+        alignment: data.text.alignment,
+        child: Text(
+          data.text.content,
+          style: TextStyle(
+            color: data.text.color,
+            fontSize: data.text.fontSize,
+          ),
+        ),
+      ),
     );
   }
 }
