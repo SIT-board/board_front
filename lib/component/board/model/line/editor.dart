178ac7d8988257113e6080fe7329225f4c0ef594
zzq <i@zhangzqs.cn>
2022-11-09 13:45:10 +0800

    feat: 编辑器添加一些直线属性

--- lib/component/board/model/line/editor.dart
+++ lib/component/board/model/line/editor.dart
@@ -2,6 +2,7 @@ import 'package:board_event_bus/board_event_bus.dart';
 import 'package:board_front/component/board/board.dart';
 import 'package:board_front/component/board/board_event.dart';
 import 'package:board_front/component/board/model/base_editor.dart';
+import 'package:board_front/util/color_picker.dart';
 import 'package:flutter/material.dart';
 
 class LineModelEditor extends StatefulWidget {
@@ -24,28 +25,63 @@ class _LineModelEditorState extends State<LineModelEditor> {
     widget.eventBus?.publish(BoardEventName.refreshModel, widget.model.id);
   }
 
-  ModelAttributeItem buildThickness() => ModelAttributeItem(
-        title: '线宽',
-        child: Slider(
-          value: modelData.thickness,
-          min: 1,
-          max: 100,
-          onChanged: (value) {
-            setState(() {
-              modelData.thickness = value;
-            });
-            refreshModel();
-          },
-        ),
-      );
-
   @override
   Widget build(BuildContext context) {
     return ModelAttribute(children: [
       ModelAttributeSection(
         title: '直线属性',
         items: [
-          buildThickness(),
+          ModelAttributeItem(
+            title: '线宽',
+            child: Slider(
+              value: modelData.thickness,
+              min: 1,
+              max: 100,
+              onChanged: (value) {
+                setState(() {
+                  modelData.thickness = value;
+                });
+                refreshModel();
+              },
+            ),
+          ),
+          ModelAttributeItem(
+            title: '直线颜色',
+            child: InkWell(
+              onTap: () async {
+                final pickedColor = await showBoardColorPicker(context);
+                if (pickedColor == null) return;
+                modelData.color = pickedColor;
+                setState(() {});
+                refreshModel();
+              },
+              child: Container(color: modelData.color, width: 50, height: 50),
+            ),
+          ),
+          ModelAttributeItem(
+            title: '实线长度',
+            child: Slider(
+              value: modelData.dashLength,
+              min: 0,
+              max: 10,
+              onChanged: (value) {
+                setState(() => modelData.dashLength = value);
+                refreshModel();
+              },
+            ),
+          ),
+          ModelAttributeItem(
+            title: '虚线长度',
+            child: Slider(
+              value: modelData.dashGapLength,
+              min: 0,
+              max: 10,
+              onChanged: (value) {
+                setState(() => modelData.dashGapLength = value);
+                refreshModel();
+              },
+            ),
+          ),
         ],
       )
     ]);
