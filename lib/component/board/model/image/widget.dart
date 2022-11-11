90cc663f5a2910d8db84f84cb8e461fb1e7d6832
zzq <i@zhangzqs.cn>
2022-11-11 12:18:47 +0800

    feat: 图片组件添加加载动画

--- lib/component/board/model/image/widget.dart
+++ lib/component/board/model/image/widget.dart
@@ -8,9 +8,18 @@ class ImageModelWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    NetworkImage(data.url).obtainKey(ImageConfiguration());
     return Image.network(
       data.url,
       fit: data.fit,
+      loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+        if (loadingProgress == null) return child;
+        int currentLength = loadingProgress.cumulativeBytesLoaded;
+        int? totalLength = loadingProgress.expectedTotalBytes;
+        return Center(
+          child: CircularProgressIndicator(value: totalLength != null ? currentLength / totalLength : null),
+        );
+      },
     );
   }
 }
