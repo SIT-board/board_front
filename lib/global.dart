b0e1c1d235c2e7a29baa9b1bd06ffcd98e54ef8e
zzq <i@zhangzqs.cn>
2022-11-12 14:13:49 +0800

    refactor: 清除无用代码

--- lib/global.dart
+++ lib/global.dart
@@ -1,14 +1,8 @@
-import 'package:board_event_bus/board_event_bus.dart';
 import 'package:board_front/storage/storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-enum EventBusEventName {
-  refreshRedoUndo,
-}
 
 /// 全局依赖对象
 class GlobalObjects {
   static late SharedPreferences sharedPreferences;
   static late BoardStorage storage;
-  static late EventBus<EventBusEventName> eventBus;
 }
