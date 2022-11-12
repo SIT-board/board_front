4870c544e778b8ce4b16ef578c38e09776a7b4fe
hwy <huawenyan22@qq.com>
2022-11-12 12:45:30 +0800

    feat: 仿照html插件实现markdown插件

--- lib/component/board/plugins/plugins.dart
+++ lib/component/board/plugins/plugins.dart
@@ -4,6 +4,7 @@ import 'freestyle/plugin_impl.dart';
 import 'html/plugin_impl.dart';
 import 'image/plugin_impl.dart';
 import 'line/plugin_impl.dart';
+import 'markdown/plugin_impl.dart';
 import 'oval/plugin_impl.dart';
 import 'rect/plugin_impl.dart';
 
@@ -11,6 +12,7 @@ export 'freestyle/plugin_impl.dart';
 export 'html/plugin_impl.dart';
 export 'image/plugin_impl.dart';
 export 'line/plugin_impl.dart';
+export 'markdown/plugin_impl.dart';
 export 'oval/plugin_impl.dart';
 export 'rect/plugin_impl.dart';
 
@@ -22,5 +24,6 @@ final defaultModelPlugins = BoardModelPluginManager(
     ImageModelPlugin(),
     FreeStyleModelPlugin(),
     HtmlModelPlugin(),
+    MarkdownModelPlugin(),
   ],
 );
